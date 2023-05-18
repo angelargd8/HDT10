06e16fdc3265d16d8f7f0452b5fcf427366cb98a
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2023-05-17 21:09:26 -0600

    floydero

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,7 +1,20 @@
 package Vista;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        //solo para probar
+        long matrizAdyacencia[][]={{0,3,4,999999999,8,999999999},
+                {999999999,0,999999999,999999999,5,999999999},
+                {999999999,999999999,0,999999999,3,999999999},
+                {999999999,999999999,999999999,0,999999999,999999999},
+                {999999999,999999999,999999999,7,0,3},
+                {999999999,999999999,999999999,2,999999999,0}};
+
+        Floyd floyd = new Floyd();
+        System.out.println(floyd.algoritmoDeFloyd(matrizAdyacencia));
+        System.out.println(floyd.CentroDelGrafo(matrizAdyacencia));
+
     }
+
 }
